7fec7858003f44cf97a80b05f017c8067aeae2d5
baltazarAvave <sebafernandez85@gmail.com>
2015-09-22 08:23:46 -0400

    agregadas librerias para levantar proyecto

--- securitysys/build/generated-sources/ap-source-output/entities/OrdenCompraDet_.java
+++ securitysys/build/generated-sources/ap-source-output/entities/OrdenCompraDet_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-09-21T22:25:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-09-22T08:19:11")
 @StaticMetamodel(OrdenCompraDet.class)
 public class OrdenCompraDet_ { 
 
