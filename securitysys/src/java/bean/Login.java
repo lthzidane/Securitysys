1fbca3a300408f68949f8dcc722a4ce6abec6771
baltazarAvave <sebafernandez85@gmail.com>
2016-06-16 22:10:25 -0400

    correccion de navegacion desde las paginas referenciales al login

--- securitysys/src/java/bean/Login.java
+++ securitysys/src/java/bean/Login.java
@@ -14,93 +14,94 @@ import javax.servlet.http.HttpSession;
 import lombok.Data;
 import bean.LoginDao;
 import javax.servlet.http.HttpServletRequest;
+
 /**
  *
- * @author 
+ * @author
  */
 @ManagedBean
 @SessionScoped
 @Data
-public class Login  implements Serializable {
+public class Login implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     private String username;
     private String password;
     private String msg;
     private boolean logueado = false;
 
-   public String getPassword() {
+    public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
- 
+
     public String getMsg() {
         return msg;
     }
- 
+
     public void setMsg(String msg) {
         this.msg = msg;
     }
- 
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
     public boolean isRenderPage() {
         String currentPage = getRequestPage();
 
         return true;
     }
-    
+
     public String getRequestPage() {
         HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
         String[] url = request.getRequestURI().split("/");
         String page = url[url.length - 1];
         return page.split("\\.")[0];
     }
-    
+
     public boolean showMenu(String menuTitle) {
         //List<UiItems> items = uiItemsFacade.findByRole(current.getIdRole().getIdRole());
-        
+
         return true;
     }
-    
-    
+
     /**
-     * Se crea una  nueva instancia para login
-     * @return 
+     * Se crea una nueva instancia para login
+     *
+     * @return
      */
-    public String validateUsernamePassword(){
+    public String validateUsernamePassword() {
         boolean valid = LoginDao.validate(username, password);
         if (valid) {
             HttpSession session = SessionBean.getSession();
-               session.setAttribute("username", username);
-                      logueado = true;
-                return "home";
-        }else{
-            FacesContext.getCurrentInstance().addMessage(null, 
-                    new FacesMessage( FacesMessage.SEVERITY_WARN, 
+            session.setAttribute("username", username);
+            logueado = true;
+            return "home";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
                             "Usuario y Contraseña invalida",
                             "Por favor ingrese Usuario y Contraseña"));
-           return "login"; 
+            return "login";
         }
     }
-    
+
     //logout
     public String logout() {
         HttpSession session = SessionBean.getSession();
         session.invalidate();
         return "login";
     }
-    } 
+}
     //public Login() {
-    //}
-    
+//}
+
